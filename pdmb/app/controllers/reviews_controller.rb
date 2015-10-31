76ada941e2ef29a47a616c1a6209743ceb5dc911
panox <panospalos@gmail.com>
2015-10-31 16:10:31 +0000

    review assigned to current user

--- pdmb/app/controllers/reviews_controller.rb
+++ pdmb/app/controllers/reviews_controller.rb
@@ -13,7 +13,7 @@ class ReviewsController < ApplicationController
   # POST /reviews
   # POST /reviews.json
   def create
-    @review = Review.new(review_params)
+    @review = current_user.reviews.new(review_params)
 
     respond_to do |format|
       if @review.save
