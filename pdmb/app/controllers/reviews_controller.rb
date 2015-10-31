aee00aa36aed62b0a94aa8f7af4d70d4fa16efa0
panox <panospalos@gmail.com>
2015-10-31 18:09:32 +0000

    review delete link

--- pdmb/app/controllers/reviews_controller.rb
+++ pdmb/app/controllers/reviews_controller.rb
@@ -49,7 +49,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to @review, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to @movie, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
