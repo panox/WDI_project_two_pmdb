f10cb874fb8d41392e02142a4f523a9e583377fe
panox <panospalos@gmail.com>
2015-10-31 17:44:55 +0000

    reviews redirect to current movie

--- pdmb/app/controllers/reviews_controller.rb
+++ pdmb/app/controllers/reviews_controller.rb
@@ -21,7 +21,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to current_user, notice: 'Review was successfully created.' }
+        format.html { redirect_to @movie, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -35,7 +35,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to current_user, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
@@ -49,7 +49,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to current_user, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to @review, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
