54d1729fbdfbdb978b81791a67a6d22d7cf6c63e
panox <panospalos@gmail.com>
2015-10-31 16:37:04 +0000

    if user deleted his reviews are removed

--- pdmb/app/models/user.rb
+++ pdmb/app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_uniqueness_of  :email
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
