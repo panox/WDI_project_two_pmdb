e3dab211dfa8c2e280d3943fbece25d003be9b15
panox <panospalos@gmail.com>
2015-11-01 16:50:41 +0000

    rating gem installed

--- pdmb/app/assets/javascripts/application.js
+++ pdmb/app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery.raty
+//= require ratyrate
 //= require turbolinks
 //= require_tree .
