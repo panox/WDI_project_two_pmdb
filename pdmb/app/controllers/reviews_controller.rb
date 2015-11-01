b08347e60ac181ce96291a0fdffe69aa0bd1147f
panox <panospalos@gmail.com>
2015-11-01 17:10:00 +0000

    delete review redirects to same page

--- pdmb/app/controllers/reviews_controller.rb
+++ pdmb/app/controllers/reviews_controller.rb
@@ -45,7 +45,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to @movie, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to :back, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
