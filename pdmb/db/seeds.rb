603df274e7e04fc150e94889ae30a8e2e810cf05
panox <panospalos@gmail.com>
2015-11-02 14:49:10 +0000

    chan wook park movies seed

--- pdmb/db/seeds.rb
+++ pdmb/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Director.create(name: "Chan-wook Park", description: "Chan-wook Park was born on August 23, 1963 in Seoul, South Korea.", country: "South Korea", picture: "http://ia.media-imdb.com/images/M/MV5BMTgyODk3MTU5OV5BMl5BanBnXkFtZTcwMTc3NjcyMQ@@._V1_UY317_CR14,0,214,317_AL_.jpg")
+
+Movie.create(name: "Oldboy", year: "2003", trailer: "2HkjrJ6IK5E", genre: "", country: "South Korea", director_id: 2)
+Movie.create(name: "Sympathy for Lady Vengeance", year: "2005", trailer: "blAgspkgcZA", genre: "", country: "South Korea", director_id: 2)
+Movie.create(name: "Sympathy for Mr. Vengeance", year: "2002", trailer: "UXdtyYRkB0k", genre: "", country: "South Korea", director_id: 2)
