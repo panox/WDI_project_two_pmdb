b4f6cc3d31474ff1e2de566ae9eae3d3ebd65d65
panox <panospalos@gmail.com>
2015-11-02 17:39:19 +0000

    Movie all query note

--- pdmb/db/seeds.rb
+++ pdmb/db/seeds.rb
@@ -6,7 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Director.create(name: "Kar Wai Wong", description: "Wong Kar-wai (born 17 July 1956) is a Hong Kong Second Wave filmmaker, internationally renowned as an auteur for his visually unique, highly stylised, emotionally resonant work", country: "China", picture: "http://ia.media-imdb.com/images/M/MV5BMTY4MTQyMjI4NV5BMl5BanBnXkFtZTcwNDk2MzQ2MQ@@._V1_UY317_CR1,0,214,317_AL_.jpg")
+Director.create(id: 1, name: "Jim Jarmusch", description: "Moved to New York City at the age of seventeen fro...", country: "USA", picture: "http://ia.media-imdb.com/images/M/MV5BMTYyODUwNDA2...") 
+Director.create(id: 2, name: "Chan-wook Park", description: "Chan-wook Park was born on August 23, 1963 in Seou...", country: "South Korea", picture: "http://ia.media-imdb.com/images/M/MV5BMTgyODk3MTU5...") 
+Director.create(id: 3, name: "Pedro Almodóvar", description: "The most internationally acclaimed Spanish filmmak...", country: "Spain", picture: "http://ia.media-imdb.com/images/M/MV5BNTE0NTk3NTMx...") 
+Director.create(id: 4, name: "Kar Wai Wong", description: "Wong Kar-wai (born 17 July 1956) is a Hong Kong Se...", country: "China", picture: "http://ia.media-imdb.com/images/M/MV5BMTY4MTQyMjI4...")
 
 Movie.create(name: "In the Mood for Love", year: "2000", trailer: "BnFjSHQFVkA", genre: "", country: "China", director_id: 4)
 Movie.create(name: "2046", year: "2004", trailer: "3Tz6KgPfeuk", genre: "", country: "China", director_id: 4)
