2cb229c7dc884add90cd1c3c5323a220425e54ca
panox <panospalos@gmail.com>
2015-11-02 18:01:02 +0000

    rating column to reviews

--- pdmb/db/schema.rb
+++ pdmb/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102172951) do
+ActiveRecord::Schema.define(version: 20151102175913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151102172951) do
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
   end
 
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
