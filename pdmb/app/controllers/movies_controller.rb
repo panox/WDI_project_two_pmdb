cefe3f2c72bf446c95ead0ddac25243a57e74ea0
panox <panospalos@gmail.com>
2015-11-02 18:54:28 +0000

    sorting search results by rating

--- pdmb/app/controllers/movies_controller.rb
+++ pdmb/app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
       @movies = Movie.all.sort { |a,b| b.rating <=> a.rating}
     elsif params[:q] != ""
       if Movie.search_by_title(params[:q]) != []
-        @movies = Movie.search_by_title(params[:q])
+        @movies = Movie.search_by_title(params[:q]).sort { |a,b| b.rating <=> a.rating}
       else
         @movies = Movie.all.sort { |a,b| b.rating <=> a.rating}
       end
