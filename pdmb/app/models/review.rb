c0e27484a8f123a938f2f64ea2003e2438bec637
panox <panospalos@gmail.com>
2015-11-03 09:34:24 +0000

    rating validation

--- pdmb/app/models/review.rb
+++ pdmb/app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
   belongs_to :user
-  validates_inclusion_of :rating, in: 1..5
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { :greater_than => 0 }
+  validates :rating, numericality: { :less_than_or_equal_to => 5 }
 end
